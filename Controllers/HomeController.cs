43dcc0419c943e6e0eb41aac3789ae644e10021c
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-21 22:43:03 +0100

    initial commit - InItialized Project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using InventoryClientApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryClientApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> LoginUser(UserInfo user)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                StringContent stringContent = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+                using (var response = await httpClient.PostAsync("https://localhost:44360/api/token", stringContent))
+                {
+                    string token = await response.Content.ReadAsStringAsync();
+                    if (token == "Invalid Credentials")
+                    {
+                        ViewBag.Message = "Incorrect UserId or Password!";
+                        return Redirect("~/Home/Index");
+                    }
+                    HttpContext.Session.SetString("JWToken", token);
+                }
+
+                return Redirect("~/Dashboard/Index");
+            }
+        }
+
+        public IActionResult Logoff()
+        {
+            HttpContext.Session.Clear();
+            return Redirect("~/Home/Index");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,103 @@
+﻿using InventoryClientApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryClientApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public static string baseUrl = "https://localhost:44360/api/products/";
+        public async Task<IActionResult> Index()
+        {
+            var products = await GetProducts();
+            return View(products);
+        }
+
+
+        [HttpGet]
+        public async Task<List<Product>> GetProducts()
+        {
+            //use the Access token to call a protected WEB API
+            var accessToken = HttpContext.Session.GetString("JWToken");
+            var url = baseUrl;
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            string jsonStr = await client.GetStringAsync(url);
+
+            var res = JsonConvert.DeserializeObject<List<Product>>(jsonStr).ToList();
+
+            return res;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProdId, ProdName, Category, UnitPrice, StockQty")] Product product)
+        {
+            //use the Access token to call a protected WEB API
+            var accessToken = HttpContext.Session.GetString("JWToken");
+            var url = baseUrl;
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            var stringContent = new StringContent(JsonConvert.SerializeObject(product), Encoding.UTF8, "application/json");
+            await client.PostAsync(url, stringContent);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if( id == null)
+            {
+                return NotFound();
+            }
+
+            var accessToken = HttpContext.Session.GetString("JWToken");
+            var url = baseUrl + id;
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            string jsonStr = await client.GetStringAsync(url);
+            var res = JsonConvert.DeserializeObject<Product>(jsonStr);
+
+            if( res == null)
+            {
+                return NotFound();
+            }
+
+            return View(res);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProdId, ProdName, Category, UnitPrice, StockQty")] Product product)
+        {
+            if (id != product.ProdId)
+            {
+                return NotFound();
+            }
+            var accessToken = HttpContext.Session.GetString("JWToken");
+            var url = baseUrl + id;
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            var stringContent = new StringContent(JsonConvert.SerializeObject(product), Encoding.UTF8, "application/json");
+            await client.PostAsync(url, stringContent);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
